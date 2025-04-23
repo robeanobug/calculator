da335b36201a0656b71b17ae37f2823915558606
robeanobug <fakeemail@gmail.com>
2025-04-22 23:18:25 -0400

    operator function added

--- calculator.js
+++ calculator.js
@@ -14,7 +14,31 @@ function divide(a, b) {
     return a / b;
 }
 
+let a = 0;
+let operator = '+'
+let b = 0;
+
+function operate(a, operator, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    } else {
+        return "ERROR";
+    }
+}
+
+
+// testing
 console.log(add(1, 2)); // expect 3
 console.log(subtract(2, 1)); // expect 1
 console.log(multiply(2, 4)); // expect 8
 console.log(divide(4, 2)); // expect 2
+console.log(operate(1, '+', 2)) // expect 3
+console.log(operate(1, '-', 2)) // expect -1
+console.log(operate(1, '*', 2)) // expect 2
+console.log(operate(1, '/', 2)) // expect 0.5
