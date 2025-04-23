583c906d65a4e212faa62e8d2ce83291f10f8dd3
robeanobug <fakeemail@gmail.com>
2025-04-22 23:07:43 -0400

    basic math operators add, subtract, multiply, and divide functions added

--- calculator.js
+++ calculator.js
@@ -1,15 +1,20 @@
-function add() {
-
+function add(a, b) {
+    return a + b;
 }
 
-function subtract() {
-
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide() {
-
+function divide(a, b) {
+    return a / b;
 }
+
+console.log(add(1, 2)); // expect 3
+console.log(subtract(2, 1)); // expect 1
+console.log(multiply(2, 4)); // expect 8
+console.log(divide(4, 2)); // expect 2
